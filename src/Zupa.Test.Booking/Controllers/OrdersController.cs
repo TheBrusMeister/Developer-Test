5a946d17605e665090588f8b191a8c6732dd8456
Matt Ridgway <matt.ridgway@zupa.co.uk>
2019-05-17 11:34:50 +0100

    Added simple API and static file

---
+++ src/Zupa.Test.Booking/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Zupa.Test.Booking.ViewModels;
+
+namespace Zupa.Test.Booking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Order>> PlaceOrder([FromBody]Basket basket)
+        {
+            var id = Guid.NewGuid();
+            return CreatedAtAction(
+                nameof(GetOrder),
+                new { id },
+                new Order { ID = id, GrossTotal = basket.Items.Sum(item => item.GrossPrice * item.Quantity) });
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Order>> GetOrder(Guid id)
+        {
+            return new Order { ID = id };
+        }
+    }
+}
